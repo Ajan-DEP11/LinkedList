917d90d2635610c22a9eb629fdfbf52c660fce78
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-14 12:55:09 +0530

    next

--- Demo1.java
+++ Demo1.java
@@ -1,5 +1,17 @@
 public class Demo1 {
     public static void main(String[] args) {
+
+        MyLinkedList list = new MyLinkedList();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        System.out.println(list.size());
+        System.out.println(list);
+        list.clear();
+        System.out.println(list.size());
+        System.out.println(list);
         
     }
 }
@@ -14,10 +26,15 @@ interface MyList{
     boolean contains(int value);
     int indexOf(int value);
     int lastIndexOf(int value);
+    
 
 }
 
 class MyLinkedList implements MyList{
+
+    private int size;
+    private MyNode head;
+    private MyNode tail;
 
       public MyLinkedList(){
 
@@ -53,6 +70,9 @@ public String toString() {
 
     @Override
     public void clear() {
+        size =0;
+        head = null;
+        tail = null;
       
         
     }
@@ -89,10 +109,27 @@ public String toString() {
 
     @Override
     public int size() {
-     
-        return 0;
+        return this.size;
+
     }
     
   
 
+}
+
+class MyNode{
+    MyNode previous;
+    MyNode next;
+    int value;
+
+    public MyNode(int value,MyNode previous){
+        this.value = value;
+        this.previous = previous;
+    }
+
+    public MyNode(int value,MyNode previous,MyNode next){
+        this.value = value;
+        this.previous = previous;
+        this.next = next;
+    }
 }
